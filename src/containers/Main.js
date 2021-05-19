e4604c64f4834f7fec7bd4ec386bb70dfb422161
Juan <pront23@gmail.com>
2021-05-20 01:42:10 +0900

    cleaned up code

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -22,12 +22,13 @@ export default class Main extends React.Component {
     this.setState({ searchField: event.target.value });
   };
   render() {
-    const filterRobots = this.state.robots.filter(robot => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+    const filterRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return (
+    return !robots.length ? (
+      <h1 className="tc">Loading...</h1>
+    ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <Searchbox searchChange={this.onSearchChange} />
