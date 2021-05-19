097ab44f0e3b1b40535430f4d4c4086ff862050a
Juan <pront23@gmail.com>
2021-05-20 00:45:50 +0900

    added fetch function

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import CardList from './CarList';
-import { robots } from './robots';
-const Main = () => {
-  return (
-    <div>
-      <h1>RoboFriends</h1>
-      <CardList robots={robots} />
-    </div>
-  );
-};
+import Searchbox from './Searchbox';
+import './Main.css';
 
-export default Main;
+export default class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: '',
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+  render() {
+    const filterRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <Searchbox searchChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />
+      </div>
+    );
+  }
+}
