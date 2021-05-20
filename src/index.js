47de3c95bf5017b5b95a01d20a1f64df2eaa5017
Juan <pront23@gmail.com>
2021-05-20 18:10:08 +0900

    installed and added redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
 import './index.css';
 
@@ -15,7 +16,10 @@ import 'tachyons';
 import { searchRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(
+  searchRobots,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
