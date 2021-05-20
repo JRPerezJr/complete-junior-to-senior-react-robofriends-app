66d1a3fec2f713d779730359e8d50c1e2ae6f27a
Juan <pront23@gmail.com>
2021-05-20 18:59:14 +0900

    added actions and reducers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
@@ -13,11 +13,16 @@ import reportWebVitals from './reportWebVitals';
 
 import 'tachyons';
 
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(
+
+const rootReducer = combineReducers({
   searchRobots,
+  requestRobots,
+});
+const store = createStore(
+  rootReducer,
   applyMiddleware(thunkMiddleware, logger)
 );
 
