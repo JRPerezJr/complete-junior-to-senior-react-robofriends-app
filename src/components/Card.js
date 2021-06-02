0bd6f358f96e772a8dd87417bdb9992d3a3fd721
Juan <pront23@gmail.com>
2021-06-03 01:33:31 +0900

    added correct query for image size

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,10 @@ const Card = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <div>
-        <img src={`https://robohash.org/${id}?200x200`} alt="robot profile" />
+        <img
+          src={`https://robohash.org/${id}?size=200x200`}
+          alt="robot profile"
+        />
       </div>
       <div>
         <h2>{name} </h2>
