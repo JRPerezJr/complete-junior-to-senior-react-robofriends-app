26c6a846f1bac88341eb2452244e087dca9dfe06
Juan <pront23@gmail.com>
2021-06-05 15:57:16 +0900

    completed Card component tests

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './Card';
 
-describe('Card component tests', () => {
+describe('<Card/> component tests', () => {
   it('sanity test', () => {
     expect(true).toBe(true);
   });
